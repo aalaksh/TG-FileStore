72d35389b3af7ddf0db510f24dbf9200cc2d377e
aalaksh <aallkkaa9451@gmail.com>
2024-09-25 19:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,14 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+    
+from aiohttp import web
+
+async def hello(request):
+    return web.Response(text="Hello, World!")
+
+app = web.Application()
+app.router.add_get('/', hello)
+
+if __name__ == '__main__':
+    web.run_app(app, host='127.0.0.1', port=8080)
